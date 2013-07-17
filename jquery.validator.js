0d3c0bb79a8b9ca5408dc9b422c472672cc4208c
Sebastian Berć <sebastian.berc@unity.pl>
2013-07-17 13:00:21 +0200

    Dodano sprawdzenie czy pole zawiera tylko znaki alfabetu

--- jquery.validator.js
+++ jquery.validator.js
@@ -20,7 +20,7 @@ String.prototype.ucfirst= function() {
 ;
 (function ( $, window, undefined ) {
     var pluginName = 'validator'
-    
+
     var helperMethods= {
         isNumber: function(n){
             n= helperMethods.removeMaskChars(n);
@@ -29,14 +29,19 @@ String.prototype.ucfirst= function() {
             }
             return false;
         },
-        
+
+        isAlpha: function(str) {
+            var preg = /^[^\s][A-z\s\.\-ÆÐƎƏƐƔĲŊŒẞÞǷȜæðǝəɛɣĳŋœĸſßþƿȝĄƁÇĐƊĘĦĮƘŁØƠŞȘŢȚŦŲƯY̨Ƴąɓçđɗęħįƙłøơşșţțŧųưy̨ƴÁÀÂÄǍĂĀÃÅǺĄÆǼǢƁĆĊĈČÇĎḌĐƊÐÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔáàâäǎăāãåǻąæǽǣɓćċĉčçďḍđɗðéèėêëěĕēęẹǝəɛġĝǧğģɣĤḤĦIÍÌİÎÏǏĬĪĨĮỊĲĴĶƘĹĻŁĽĿʼNŃN̈ŇÑŅŊÓÒÔÖǑŎŌÕŐỌØǾƠŒĥḥħıíìiîïǐĭīĩįịĳĵķƙĸĺļłľŀŉńn̈ňñņŋóòôöǒŏōõőọøǿơœŔŘŖŚŜŠŞȘṢẞŤŢṬŦÞÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒŕřŗſśŝšşșṣßťţṭŧþúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓ]+$/i;
+            return preg.test(str);
+        },
+
         removeMaskChars: function(str){
             if(!str){
                 return '';
             }
             return String(str).replace(/ /g,'').replace(/-/g, '').replace(/_/g, '');
         },
-    
+
         validationFailed: function(testName, validator){
             validator.testsSummary[testName]=false;
             return false;
@@ -46,7 +51,7 @@ String.prototype.ucfirst= function() {
             validator.testsSummary[testName]=true;
             return true;
         },
-        
+
         setClass: function(validator){
             validator.$element.removeClass(validator.options.errorClass);
             validator.$element.removeClass(validator.options.successClass);
@@ -56,7 +61,7 @@ String.prototype.ucfirst= function() {
                 validator.$element.addClass(validator.options.errorClass);
             }
         },
-        
+
         addToErrorHolder: function(validator){
             if(!validator.options.errorHolderId){
                 return;
@@ -73,7 +78,7 @@ String.prototype.ucfirst= function() {
             }else if(validator.options.errorHolderApendName){
                 errorHolderName+=validator.$element.attr('name');
             }
-            
+
             var holder= $('#'+errorHolderName);
             for(output in validator.testsSummary){
                 if(validator.testsSummary[output]===false){
@@ -88,7 +93,7 @@ String.prototype.ucfirst= function() {
             }
             holder.appendChild(title)
         },
-        
+
         addTitleForTooltip: function(validator){
             var title='';
             for(output in validator.testsSummary){
@@ -113,7 +118,7 @@ String.prototype.ucfirst= function() {
             }
         }
     }
-    
+
     var document = window.document
     var defaults = {
         required: false,
@@ -121,6 +126,7 @@ String.prototype.ucfirst= function() {
         checkMaxLength: false,
         checkLength: false,
         checkIsNumber: false,
+        checkIsAlpha: false,
         checkEmail: false,
         checkPhone: false,
         equalsTo:false,
@@ -142,19 +148,23 @@ String.prototype.ucfirst= function() {
         tooltip:true,
         debug:false
     };
+
     var defaultRegexp={
         email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|pl|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/,
         postcode: /[0-9]{2}-[0-9]{3}/,
         personalId: /[a-zA-Z]{3}[0-9]{6}/
     }
-    
+
+    /*'*/
+
     var validatorMessages= {
         pl: {
             required: 'Pole jest wymagane.',
             checkMinLength: 'Pole wymaga podania minimalnie {1} znaków.',
             checkMaxLength: 'Pole wymaga podania maksymalnie {1} znaków.',
             checkLength: 'Pole wymaga podania dokładnie {1} znaków.',
-            checkIsNumber: 'Pole wymaga podania liczby.',
+            checkIsNumber: 'Pole wymaga podania tylko liczb.',
+            checkIsAlpha: 'Pole wymaga podania tylko liter.',
             checkEmail: 'Adres email jest niepoprawny.',
             checkPhone: 'Numer telefonu jest niepoprawny.',
             checkPostcode: 'Kod pocztowy jest niepoprawny',
@@ -163,10 +173,10 @@ String.prototype.ucfirst= function() {
             checkPesel: 'Pesel jest niepoprawny',
             checkPersonalId:'Seria i numer dowodu osobistego są niepoprawne',
             checkRegexp: 'Pole wymaga formatu "{1}"',
-            equalsTo: 'Pole ma inn\u0105 warto�� ni� pole "{2}"'
+            equalsTo: 'Pole ma inną wartość niż pole "{2}"'
         }
     }
-     
+
     var validationMethods= {
         required: function(validator){
             if(validator.$element.is(':checkbox')){
@@ -190,7 +200,7 @@ String.prototype.ucfirst= function() {
                 return helperMethods.validationFailed('checkMinLength', validator);
             }
             return helperMethods.validationSuccess('checkMinLength', validator);
-            
+
         },
         checkMaxLength: function(validator){
             if(helperMethods.removeMaskChars(validator.options.checkMaxLength)==0){
@@ -219,6 +229,15 @@ String.prototype.ucfirst= function() {
             }
             return helperMethods.validationSuccess('checkIsNumber', validator);
         },
+        checkIsAlpha: function(validator){
+            if(helperMethods.removeMaskChars(validator.$element.val()).length==0){
+                return helperMethods.validationSuccess('checkIsAlpha', validator);
+            }
+            if(!helperMethods.isAlpha(helperMethods.removeMaskChars(validator.$element.val()))){
+                return helperMethods.validationFailed('checkIsAlpha', validator);
+            }
+            return helperMethods.validationSuccess('checkIsAlpha', validator);
+        },
         checkEmail: function(validator){
             if(validator.$element.val().length==0){
                 return helperMethods.validationSuccess('checkEmail', validator);
@@ -227,7 +246,7 @@ String.prototype.ucfirst= function() {
                 return helperMethods.validationFailed('checkEmail', validator);
             }
             return helperMethods.validationSuccess('checkEmail', validator);
-            
+
         },
         checkPhone: function(validator){
             if(helperMethods.removeMaskChars(validator.$element.val()).length==0){
@@ -242,7 +261,7 @@ String.prototype.ucfirst= function() {
                 return helperMethods.validationSuccess('checkPhone', validator);
             }
             return helperMethods.validationFailed('checkPhone', validator);
-            
+
         },
         checkPostcode: function(validator){
             if(validator.$element.val().length==0){
@@ -302,39 +321,39 @@ String.prototype.ucfirst= function() {
             if (value.length!=11 || !helperMethods.isNumber(value)){
                 return false;
             }
-            var arrSteps = new Array(1, 3, 7, 9, 1, 3, 7, 9, 1, 3); 
+            var arrSteps = new Array(1, 3, 7, 9, 1, 3, 7, 9, 1, 3);
             var intSum = 0;
             for (var i = 0; i < 10; i++){
-                intSum += arrSteps[i] * value[i]; 
+                intSum += arrSteps[i] * value[i];
             }
-            var intFinal = 10 - intSum % 10; 
+            var intFinal = 10 - intSum % 10;
             var intControlNr = (intFinal == 10)?0:intFinal;
             if (intControlNr == value[10]){
                 return true;
             }
             return false;
         },
-        
+
         checkPersonalId: function(validator){
             if(helperMethods.removeMaskChars(validator.$element.val()).length==0){
                 return helperMethods.validationSuccess('checkPersonalId', validator);
             }
-            
+
             var value= helperMethods.removeMaskChars(validator.$element.val());
-                        
+
             if(value.length!=9){
                 return helperMethods.validationFailed('checkPersonalId', validator);
-            } 
-                        
+            }
+
             if(!defaultRegexp.personalId.test(validator.$element.val())){
                 return helperMethods.validationFailed('checkPersonalId', validator);
             }
-                                   
-            var charArray = {"A":"10","B":"11","C":"12","D":"13","E":"14","F":"15","G":"16","H":"17","I":"18","J":"19","K":"20","L":"21","M":"22","N":"23","O":"24","P":"25","Q":"26","R":"27","S":"28","T":"29","U":"30","V":"31","W":"32","X":"33","Y":"34","Z":"35"};           
+
+            var charArray = {"A":"10","B":"11","C":"12","D":"13","E":"14","F":"15","G":"16","H":"17","I":"18","J":"19","K":"20","L":"21","M":"22","N":"23","O":"24","P":"25","Q":"26","R":"27","S":"28","T":"29","U":"30","V":"31","W":"32","X":"33","Y":"34","Z":"35"};
             var weight = [7,3,1,9,7,3,1,7,3];
             var sum=0;
             for(var i = 0; i < value.length; i++)
-                {   
+                {
                     if(i<=2){
                         sum += parseInt(charArray[value[i].toUpperCase()])*weight[i];
                     }
@@ -342,14 +361,14 @@ String.prototype.ucfirst= function() {
                         sum += parseInt(value[i])*weight[i];
                     }
                 }
-            
+
             if(sum%10===0){
-               return helperMethods.validationSuccess('checkPersonalId', validator); 
+               return helperMethods.validationSuccess('checkPersonalId', validator);
             }
             return helperMethods.validationFailed('checkPersonalId', validator);
             return false;
         },
-        
+
         checkRegexp: function(validator){
             if(validator.$element.val().length==0){
                 return helperMethods.validationSuccess('checkRegexp', validator);
@@ -359,7 +378,7 @@ String.prototype.ucfirst= function() {
             }
             return helperMethods.validationSuccess('checkRegexp', validator);
         },
-        
+
         equalsTo: function(validator){
             if(validator.$element.val().length==0 || !$('#'+validator.options.equalsTo).length){
                 return helperMethods.validationSuccess('equalsTo', validator);
@@ -370,7 +389,7 @@ String.prototype.ucfirst= function() {
             return helperMethods.validationSuccess('equalsTo', validator);
         }
     }
-    
+
     var controlerMethods= {
         remove: function(validator){
             delete validator;
@@ -397,7 +416,7 @@ String.prototype.ucfirst= function() {
                     console.debug(validator);
                 }
             }
-        } 
+        }
     }
 
     function validator( element, options ) {
@@ -420,9 +439,9 @@ String.prototype.ucfirst= function() {
     }
 
     validator.prototype.init = function () {
-    
+
     };
-    
+
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
@@ -434,7 +453,7 @@ String.prototype.ucfirst= function() {
             }
         });
     }
-    
+
     $.fn.validate= function(){
         var options= {
             method:'validate'
@@ -449,7 +468,7 @@ String.prototype.ucfirst= function() {
             }
         });
     }
-    
+
     $.fn.removeAllValidation= function(){
         var options= {
             method:'remove'
@@ -463,7 +482,7 @@ String.prototype.ucfirst= function() {
             $.data(this, 'plugin_' + pluginName, new validator( this, extended ));
         });
     }
-    
+
     $.fn.isValid= function(){
         var options= {
             method:'validate'
@@ -485,25 +504,25 @@ String.prototype.ucfirst= function() {
         });
         return valid;
     }
-    
+
     $.fn.validateForm= function(){
         return this.each(function(){
             $(this).find('input, textarea, select').not(':button').validate();
         });
     }
-    
+
     $.fn.validateFieldset= function(){
         return this.each(function(){
             $(this).find('input, textarea, select').not(':button').validate();
         });
     }
-    
+
     $.fn.removeValidation= function(methodName){
         var options= {};
         options[methodName]=false;
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                
+
             }
             else{
                 var extended= $.extend({},$.data(this, 'plugin_' + pluginName).options,options);
